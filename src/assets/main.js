31914b770febf2fa831201af9bd9c23df399008e
BrainJelly <jsfritzler@hotmail.com>
2016-11-21 15:24:08 -0700

    finish main.js file :)

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,23 +1,18 @@
 $(function() {
 
-  // your code will go here
-  //https://www.codeschool.com/users/2233236
-
-  //Make an Ajax request to the Report Card URL you found on CodeSchool.com
-  //Set the dataType as jsonp
-  //Pass a success callback
-
- //By now, our Ajax call will have returned a response object that contains an array field called response.courses.completed.
- //Let's add one div element per item in the courses.completed array inside of the #badges element.
- //Add the CSS class course to each div element we add to #badges.
-
   $.ajax({
     url: 'https://www.codeschool.com/users/2233236.json',
-    dataType: 'json',
+    dataType: 'jsonp',
     success: function(response) {
       // handle response
+      console.log(response);
       for(let course in response.courses.completed) {
-
+        //console.log(response.courses.completed[course].title);
+        //console.log(response.courses.completed[course].url);
+        $('#badges').append('<div class="course"><h3>' + response.courses.completed[course].title + '</h3>' +
+        '<img src= "'+ response.courses.completed[course].badge + '"/>' +
+        '<a class="btn btn-primary" target="_blank" href=" '+ response.courses.completed[course].url + '"'+ '>See Course </a>' +
+        '</div>');
       }
 
     }
